f34073468b9aa9f2cf34e24715169ad550272062
abhilashaam <abhilashaamgowda@gmail.com>
2021-03-22 12:26:07 +0530

    update in master

--- main.go
+++ main.go
@@ -6,5 +6,8 @@ func main() {
 	fmt.Printf("Remote commit")
 	fmt.Printf("commit-1")
 	fmt.Printf("commit-2")
+	fmt.Printf("commit-3")
 	fmt.Printf("trying to squash")
+	fmt.Printf("update in master")
+
 }
